e0a09102fd7d1bb62b0c569b634dabc62641abd7
guhan1203 <guhanramu1203@gmail.com>
2023-08-16 10:47:37 +0530

    tgbgnhmj,

---
+++ reactstore/src/pages/Index.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Carousel from '../index components/Carousel'
+
+function Index() {
+  return (
+    <div>
+      <Carousel / >
+    </div>
+  )
+}
+
+export default Index
