76f56c7dc64cd004b6ba609efd03ac728cb99966
kamalnah2003 <54796340+kamalnah2003@users.noreply.github.com>
2023-08-17 11:25:59 +0530

    new

--- reactstore/src/components/NavigationBar/NavigationBar.jsx
+++ reactstore/src/components/NavigationBar/NavigationBar.jsx
@@ -1,21 +1,27 @@
-
-import React from 'react';
-import { Link } from 'react-router-dom';
-import Display from '../../pages/tv and display/Display';
+import React from "react";
+import { Link } from "react-router-dom";
+import Display from "../../pages/tv and display/Display";
 
 const NavigationBar = () => {
   return (
     <div className="d-grid">
       <div className="row-cols-1">
         <div className="col-12">
-          <nav className="navbar navbar-expand-lg p-0 bg-body-tertiary" style={{ margin: '0%' }}>
-            <div className="container-fluid my-0" style={{ fontSize: '80%' }}>
-              <a className="ps-5 navbar-brand me-3" href="index.html">
-                <img src="src/OnePlus-Logo-new.png" style={{ width: '100px' }} alt="" />
+          <nav
+            className="navbar navbar-expand-lg p-0 bg-body-tertiary"
+            style={{ margin: "0%" }}
+          >
+            <div className="container-fluid my-0" style={{ fontSize: "80%" }}>
+              <a className="py-2 ps-5 navbar-brand " href="/">
+                <img
+                  src="https://seeklogo.com/images/O/oneplus-logo-70A6B4FAEA-seeklogo.com.png"
+                  style={{ width: "100px" }}
+                  alt=""
+                />
               </a>
 
               <button
-                style={{ boxShadow: 'none', border: 'none' }}
+                style={{ boxShadow: "none", border: "none" }}
                 className="navbar-toggler"
                 type="button"
                 data-bs-toggle="collapse"
@@ -26,19 +32,20 @@ const NavigationBar = () => {
               >
                 <span className="navbar-toggler-icon"></span>
               </button>
-              <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                <ul className="navbar-nav mb-2 mb-lg-0 text-nowrap">
-                  <li className="nav-item">
-                    <Link to = "/mobile">Phone</Link>
-                    
-                    
+              <div
+                className="collapse navbar-collapse pe-5 pt-2 "
+                id="navbarSupportedContent"
+              >
+                <ul className="navbar-nav mb-2 mb-lg-0 text-nowrap pe-5" style={{fontWeight:'600'}}>
+                  <li className="nav-item " >
+                    <a class="nav-link " href="/mobile">
+                      Mobile
+                    </a>
                   </li>
                   <li className="nav-item">
-                    <Link to="/display">
-                    <a className="nav-link" href="tv-display.html">
+                    <a className="nav-link" href="/display">
                       TV/Display
                     </a>
-                    </Link>
                   </li>
                   <li className="nav-item">
                     <a className="nav-link" href="tv-display.html">
@@ -58,38 +65,46 @@ const NavigationBar = () => {
                 </ul>
 
                 <div className="d-flex ms-auto">
-                  <div className="d-flex ms-auto">
-                    <div className="d-flex me-5">
+                  <div className="d-flex  ms-auto">
+                    <div className="d-flex me-3 ">
                       <div className="dropdown text-end">
-                        <a href="#"
+                        <a
+                          href="#"
                           className="display-1 link-body-emphasis text-decoration-none dropdown-header"
                           data-bs-toggle="dropdown"
                           aria-expanded="false"
                         >
-                          <span alt="login" style={{ width: '10px' }} className="material-symbols-outlined rounded-circle mx-3 my-0">
+                          <span
+                            alt="login"
+                            style={{ width: "10px" }}
+                            className="material-symbols-outlined rounded-circle  my-0"
+                          >
                             search
                           </span>
                         </a>
                         <ul className="dropdown-menu text-small">
                           <li>
                             <a className="dropdown-item" href="#">
-                              <input className="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search" />
+                              <input
+                                className="form-control mr-sm-2"
+                                type="search"
+                                placeholder="Search"
+                                aria-label="Search"
+                              />
                             </a>
                           </li>
                         </ul>
                       </div>
 
-                      <a
-                        href="#"
-                        className="d-block link-body-emphasis text-decoration-none mx-2"
-                        aria-expanded="false"
-                      >
-                        <img src="src/shopping-cart.png" alt="cart" width="25" height="25" className="rounded-circle" />
-                      </a>
                       <ul className="dropdown-menu text-small">
                         <li>
                           <a className="dropdown-item" href="#">
-                            <input className="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search" />
+                            <input
+                              className="form-control mr-sm-2"
+                              type="search"
+                              placeholder="Search"
+                              aria-label="Search"
+                            />
                           </a>
                         </li>
                       </ul>
@@ -100,7 +115,9 @@ const NavigationBar = () => {
                       className="d-block link-body-emphasis text-decoration-none mx-2"
                       aria-expanded="false"
                     >
-                      <img src="src/shopping-cart.png" alt="cart" width="25" height="25" className="rounded-circle" />
+                      <span class="material-symbols-outlined">
+                        shopping_cart
+                      </span>
                     </a>
                     <div className="dropdown text-end">
                       <a
@@ -109,7 +126,11 @@ const NavigationBar = () => {
                         data-bs-toggle="dropdown"
                         aria-expanded="false"
                       >
-                        <span alt="login" style={{ width: '10px' }} className="material-symbols-outlined rounded-circle mx-1">
+                        <span
+                          alt="login"
+                          style={{ width: "10px" }}
+                          className="material-symbols-outlined rounded-circle mx-1"
+                        >
                           person
                         </span>
                       </a>
