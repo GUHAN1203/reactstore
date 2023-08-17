5923818b6c718876bfcacd69af22085ad4a950ef
rahulrakesh17 <rahulrakesh1702@gmail.com>
2023-08-17 11:40:03 +0530

    mhv

--- reactstore/src/pages/Home/Index.jsx
+++ reactstore/src/pages/Home/Index.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import Carousel from './assets/index components/Carousel';
-
+import Picks from './assets/index components/Picks';
 
 function Index() {
   return (
     <div>
       <Carousel/>
+      <Picks/>
     </div>
   )
 }
