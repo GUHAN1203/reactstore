8775ecb293cca51fb8aed59ded05edc149df0496
guhan1203 <guhanramu1203@gmail.com>
2023-08-17 11:59:54 +0530

    mobiles

--- reactstore/src/pages/mobile/Mobile.jsx
+++ reactstore/src/pages/mobile/Mobile.jsx
@@ -6,21 +6,21 @@ function Mobile() {
     <>
     
     <div className="row">
-      <div className="col-2"><Filter/></div>
+      <div className="col-3"><Filter/></div>
       
    
 
-        <div class="col-lg-10 col "> 
+        <div class="col-lg-9 col "> 
 
       
-<div class=" row row-cols-2  row-cols-md-2 row-cols-lg-4  g-3 me-0" id="mob">
+<div class=" row row-cols-2  row-cols-md-3 row-cols-lg-3  g-3 me-0" id="mob">
         
         <div class="col">
           <div className=" card rounded-0 border-white ">
-            <img src="https://image01-in.oneplus.net/india-oneplus-statics-file/epb/202306/02/Z8drqdOKU2JlZyl5.png?x-amz-process=image/format,webp/quality,Q_80" />
+            <img src="https://image01-in.oneplus.net/india-oneplus-statics-file/epb/202304/20/wC5oeskT8aEtFHpn.png?x-amz-process=image/format,webp/quality,Q_80" />
 
             <div class="card-body bg-white ">
-              <p class="card-header bg-white"><b>ONEPLUS 11 5G</b></p>
+              <p class="card-header bg-white"><b>ONEPLUS Pad</b></p>
               <p> 8 GB RAM + 128 GB Storage</p>
               <div class="d-grid gap-2 col-12 mx-auto">
                 <a href="phonemodel.html" ><button class="btn btn-dark rounded-0" type="button" >Buy</button></a>
@@ -30,8 +30,92 @@ function Mobile() {
                   
                 </div>
                 
-                <small class="text-decoration-line-through text-secondary">₹45,000</small>
+                <small class="text-decoration-line-through text-secondary">₹37,999</small>
                 <small class="text-black text-primary">₹40,000</small>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="col">
+          <div className=" card rounded-0 border-white ">
+            <img src="https://image01-in.oneplus.net/india-oneplus-statics-file/epb/202304/03/NZuWi2WsYLtOAgER.png?x-amz-process=image/format,webp/quality,Q_80" />
+
+            <div class="card-body bg-white ">
+              <p class="card-header bg-white"><b>ONEPLUS Nord CE3 5G</b></p>
+              <p> 8 GB RAM + 128 GB Storage</p>
+              <div class="d-grid gap-2 col-12 mx-auto">
+                <a href="phonemodel.html" ><button class="btn btn-dark rounded-0" type="button" >Buy</button></a>
+              </div>
+              <div class="d- justify-content-between align-items-center ">
+                <div class="btn-group">
+                  
+                </div>
+                
+                <small class="text-decoration-line-through text-secondary">₹20,999</small>
+                <small class="text-black text-primary">₹19,000</small>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="col">
+          <div className=" card rounded-0 border-white ">
+            <img src="https://image01-in.oneplus.net/india-oneplus-statics-file/epb/202306/26/B4daH1IvptTKFpAx.png?x-amz-process=image/format,webp/quality,Q_80" />
+
+            <div class="card-body bg-white ">
+              <p class="card-header bg-white"><b>ONEPLUS Nord 3 5G</b></p>
+              <p> 12 GB RAM + 256 GB Storage</p>
+              <div class="d-grid gap-2 col-12 mx-auto">
+                <a href="phonemodel.html" ><button class="btn btn-dark rounded-0" type="button" >Buy</button></a>
+              </div>
+              <div class="d- justify-content-between align-items-center ">
+                <div class="btn-group">
+                  
+                </div>
+                
+                <small class="text-decoration-line-through text-secondary">₹34,999</small>
+                <small class="text-black text-primary">₹37,999</small>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="col">
+          <div className=" card rounded-0 border-white ">
+            <img src="https://image01-in.oneplus.net/india-oneplus-statics-file/epb/202302/02/kjgeQwHFysCxzQBY.png?x-amz-process=image/format,webp/quality,Q_80" />
+
+            <div class="card-body bg-white ">
+              <p class="card-header bg-white"><b>ONEPLUS 11R 5G</b></p>
+              <p> 8 GB RAM + 128 GB Storage</p>
+              <div class="d-grid gap-2 col-12 mx-auto">
+                <a href="phonemodel.html" ><button class="btn btn-dark rounded-0" type="button" >Buy</button></a>
+              </div>
+              <div class="d- justify-content-between align-items-center ">
+                <div class="btn-group">
+                  
+                </div>
+                
+                <small class="text-decoration-line-through text-secondary">₹43,999</small>
+                <small class="text-black text-primary">₹39,999</small>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="col">
+          <div className=" card rounded-0 border-white ">
+            <img src="https://image01-in.oneplus.net/india-oneplus-statics-file/epb/202211/30/XNOpzegrPhsZk4L6.png?x-amz-process=image/format,webp/quality,Q_80" />
+
+            <div class="card-body bg-white ">
+              <p class="card-header bg-white"><b>ONEPLUS Nord CE2 5G</b></p>
+              <p> 8 GB RAM + 128 GB Storage</p>
+              <div class="d-grid gap-2 col-12 mx-auto">
+                <a href="phonemodel.html" ><button class="btn btn-dark rounded-0" type="button" >Buy</button></a>
+              </div>
+              <div class="d- justify-content-between align-items-center ">
+                <div class="btn-group">
+                  
+                </div>
+                
+                <small class="text-decoration-line-through text-secondary">₹20,999</small>
+                <small class="text-black text-primary">₹18,999</small>
               </div>
             </div>
           </div>
@@ -59,11 +143,11 @@ function Mobile() {
         </div>
         <div class="col">
           <div className=" card rounded-0 border-white ">
-            <img src="https://image01-in.oneplus.net/india-oneplus-statics-file/epb/202306/02/Z8drqdOKU2JlZyl5.png?x-amz-process=image/format,webp/quality,Q_80" />
+            <img src="https://image01-in.oneplus.net/india-oneplus-statics-file/epb/202212/01/GPDCP8d7M9W6jKyN.png?x-amz-process=image/format,webp/quality,Q_80" />
 
             <div class="card-body bg-white ">
-              <p class="card-header bg-white"><b>ONEPLUS 11 5G</b></p>
-              <p> 8 GB RAM + 128 GB Storage</p>
+              <p class="card-header bg-white"><b>ONEPLUS 9 5G</b></p>
+              <p> 12 GB RAM + 256 GB Storage</p>
               <div class="d-grid gap-2 col-12 mx-auto">
                 <a href="phonemodel.html" ><button class="btn btn-dark rounded-0" type="button" >Buy</button></a>
               </div>
@@ -72,18 +156,18 @@ function Mobile() {
                   
                 </div>
                 
-                <small class="text-decoration-line-through text-secondary">₹45,000</small>
-                <small class="text-black text-primary">₹40,000</small>
+                <small class="text-decoration-line-through text-secondary">₹45,999</small>
+                <small class="text-black text-primary">₹42,999</small>
               </div>
             </div>
           </div>
         </div>
         <div class="col">
           <div className=" card rounded-0 border-white ">
-            <img src="https://image01-in.oneplus.net/india-oneplus-statics-file/epb/202306/02/Z8drqdOKU2JlZyl5.png?x-amz-process=image/format,webp/quality,Q_80" />
+            <img src="https://image01-in.oneplus.net/india-oneplus-statics-file/epb/202212/01/OFIkb1ghqC4F8MMd.png?x-amz-process=image/format,webp/quality,Q_80" />
 
             <div class="card-body bg-white ">
-              <p class="card-header bg-white"><b>ONEPLUS 11 5G</b></p>
+              <p class="card-header bg-white"><b>ONEPLUS 9R 5G</b></p>
               <p> 8 GB RAM + 128 GB Storage</p>
               <div class="d-grid gap-2 col-12 mx-auto">
                 <a href="phonemodel.html" ><button class="btn btn-dark rounded-0" type="button" >Buy</button></a>
@@ -93,92 +177,8 @@ function Mobile() {
                   
                 </div>
                 
-                <small class="text-decoration-line-through text-secondary">₹45,000</small>
-                <small class="text-black text-primary">₹40,000</small>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div class="col">
-          <div className=" card rounded-0 border-white ">
-            <img src="https://image01-in.oneplus.net/india-oneplus-statics-file/epb/202306/02/Z8drqdOKU2JlZyl5.png?x-amz-process=image/format,webp/quality,Q_80" />
-
-            <div class="card-body bg-white ">
-              <p class="card-header bg-white"><b>ONEPLUS 11 5G</b></p>
-              <p> 8 GB RAM + 128 GB Storage</p>
-              <div class="d-grid gap-2 col-12 mx-auto">
-                <a href="phonemodel.html" ><button class="btn btn-dark rounded-0" type="button" >Buy</button></a>
-              </div>
-              <div class="d- justify-content-between align-items-center ">
-                <div class="btn-group">
-                  
-                </div>
-                
-                <small class="text-decoration-line-through text-secondary">₹45,000</small>
-                <small class="text-black text-primary">₹40,000</small>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div class="col">
-          <div className=" card rounded-0 border-white ">
-            <img src="https://image01-in.oneplus.net/india-oneplus-statics-file/epb/202306/02/Z8drqdOKU2JlZyl5.png?x-amz-process=image/format,webp/quality,Q_80" />
-
-            <div class="card-body bg-white ">
-              <p class="card-header bg-white"><b>ONEPLUS 11 5G</b></p>
-              <p> 8 GB RAM + 128 GB Storage</p>
-              <div class="d-grid gap-2 col-12 mx-auto">
-                <a href="phonemodel.html" ><button class="btn btn-dark rounded-0" type="button" >Buy</button></a>
-              </div>
-              <div class="d- justify-content-between align-items-center ">
-                <div class="btn-group">
-                  
-                </div>
-                
-                <small class="text-decoration-line-through text-secondary">₹45,000</small>
-                <small class="text-black text-primary">₹40,000</small>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div class="col">
-          <div className=" card rounded-0 border-white ">
-            <img src="https://image01-in.oneplus.net/india-oneplus-statics-file/epb/202306/02/Z8drqdOKU2JlZyl5.png?x-amz-process=image/format,webp/quality,Q_80" />
-
-            <div class="card-body bg-white ">
-              <p class="card-header bg-white"><b>ONEPLUS 11 5G</b></p>
-              <p> 8 GB RAM + 128 GB Storage</p>
-              <div class="d-grid gap-2 col-12 mx-auto">
-                <a href="phonemodel.html" ><button class="btn btn-dark rounded-0" type="button" >Buy</button></a>
-              </div>
-              <div class="d- justify-content-between align-items-center ">
-                <div class="btn-group">
-                  
-                </div>
-                
-                <small class="text-decoration-line-through text-secondary">₹45,000</small>
-                <small class="text-black text-primary">₹40,000</small>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div class="col">
-          <div className=" card rounded-0 border-white ">
-            <img src="https://image01-in.oneplus.net/india-oneplus-statics-file/epb/202306/02/Z8drqdOKU2JlZyl5.png?x-amz-process=image/format,webp/quality,Q_80" />
-
-            <div class="card-body bg-white ">
-              <p class="card-header bg-white"><b>ONEPLUS 11 5G</b></p>
-              <p> 8 GB RAM + 128 GB Storage</p>
-              <div class="d-grid gap-2 col-12 mx-auto">
-                <a href="phonemodel.html" ><button class="btn btn-dark rounded-0" type="button" >Buy</button></a>
-              </div>
-              <div class="d- justify-content-between align-items-center ">
-                <div class="btn-group">
-                  
-                </div>
-                
-                <small class="text-decoration-line-through text-secondary">₹45,000</small>
-                <small class="text-black text-primary">₹40,000</small>
+                <small class="text-decoration-line-through text-secondary">₹41,999</small>
+                <small class="text-black text-primary">₹39,999</small>
               </div>
             </div>
           </div>
@@ -188,7 +188,7 @@ function Mobile() {
             <img src="https://image01-in.oneplus.net/india-oneplus-statics-file/epb/202306/02/Z8drqdOKU2JlZyl5.png?x-amz-process=image/format,webp/quality,Q_80" />
 
             <div class="card-body bg-white ">
-              <p class="card-header bg-white"><b>ONEPLUS 11 5G</b></p>
+              <p class="card-header bg-white"><b>ONEPLUS 10 Pro 5G</b></p>
               <p> 8 GB RAM + 128 GB Storage</p>
               <div class="d-grid gap-2 col-12 mx-auto">
                 <a href="phonemodel.html" ><button class="btn btn-dark rounded-0" type="button" >Buy</button></a>
@@ -198,8 +198,8 @@ function Mobile() {
                   
                 </div>
                 
-                <small class="text-decoration-line-through text-secondary">₹45,000</small>
-                <small class="text-black text-primary">₹40,000</small>
+                <small class="text-decoration-line-through text-secondary">₹70,000</small>
+                <small class="text-black text-primary">₹66,000</small>
               </div>
             </div>
           </div>
