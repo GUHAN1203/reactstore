0387ace98c78bd212dc647b4d92075cdb1eed65a
kamalnah2003 <54796340+kamalnah2003@users.noreply.github.com>
2023-08-21 13:20:45 +0530

    new

--- reactstore/src/pages/login/Loginform/Login.jsx
+++ reactstore/src/pages/login/Loginform/Login.jsx
@@ -1,8 +1,10 @@
 import React from "react";
+import {FaTimes} from "react-icons/fa"
 
 function Login() {
   return (
     <section className="vh-100" style={{ backgroundColor: 'gray' }}>
+      <FaTimes/>
     <div className="container py-5 h-100">
       <div className="row d-flex justify-content-center align-items-center h-100">
         <div className="col-12 col-md-8 col-lg-6 col-xl-5">
