45bf5bc4b32469598e02e6b580e6b17a61424f86
rahulrakesh17 <rahulrakesh1702@gmail.com>
2023-08-22 14:35:36 +0530

    trdr

--- backend/router/products.js
+++ backend/router/products.js
@@ -2,7 +2,21 @@ const express=require("express");
 const router = express.Router();
 const multer = require("multer");
 const path = require("path");
+const Products = require("../model/product.model")
 
-router.post("/add-product",(req,res)=>{
-    
+
+const Storage = multer.diskStorage({
+    destination: function(req,file,cb){
+        cb(null,"public/image")
+    },
+    filename: function(req,file,cb){
+        cb(null.file.fieldname+"-"+Date.now()+path.extname(file.originalname))
+    }
 })
+
+const upload = multer({storage:Storage})
+router.post("/add-product",upload.single("imageFile"),(req,res)=>{
+
+})
+
+module.exports=router;
