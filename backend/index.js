62cb603b7d771db92f57fb8e9499eb847864333c
rahulrakesh17 <rahulrakesh1702@gmail.com>
2023-08-24 09:25:15 +0530

    dwkn

--- backend/index.js
+++ backend/index.js
@@ -2,12 +2,16 @@ const express = require("express");
 
 const cors = require("cors");
 const mongoose = require("mongoose");
+const bodyParser = require("body-parser")
+const productRouter =require ("./router/products")
 
 const app = express();
 app.use(cors());
+app.use(bodyParser.urlencoded({extended:false}))
+app.use(bodyParser.json())
 require("dotenv").config();
 const url = process.env.ATLAS_URL;
-
+app.use("/product",productRouter)
 app.use("/", (req, res)=>{
     res.send("Hello Node");
 })
@@ -20,4 +24,5 @@ mongoose.connect(url)
 .catch((err) =>{
     console.log(err)
 })
+
 
