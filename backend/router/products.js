27e3d1e6be3506a82c450f5414e6ac06b3f6850e
rahulrakesh17 <rahulrakesh1702@gmail.com>
2023-08-24 08:47:45 +0530

    noire

--- backend/router/products.js
+++ backend/router/products.js
@@ -1,36 +1,41 @@
-const express=require("express");
+const express = require("express");
 const router = express.Router();
 const multer = require("multer");
 const path = require("path");
-const Products = require("../model/product.model")
-
+const Products = require("../model/product.model");
 
 const Storage = multer.diskStorage({
-    destination: function(req,file,cb){
-        cb(null,"public/image")
+    destination: function(req, file, cb){
+        cb(null, "public/images")
     },
-    filename: function(req,file,cb){
-        cb(null.file.fieldname+"-"+Date.now()+path.extname(file.originalname))
+    filename: function(req, file, cb){
+        cb(null, file.fieldname+"-"+Date.now()+path.extname(file.originalname))
     }
+
 })
 
-const upload = multer({storage:Storage})
-router.post("/add-product",upload.single("imageFile"),(req,res)=>{
-    const title=req.body.title;
-    const price=req.body.price;
-    const description=req.body.description;
-    const imgFile=req.file.filename; 
-    const newProduct =new Products({
-        title:title,
-        price:price,
-        description:description,
-        imgPath:imgFile
+const upload = multer({storage: Storage})
 
+router.post("/add-product", upload.single("imageFile"), (req, res)=>{
+    const title = req.body.title;
+    const price = req.body.price;
+    const description = req.body.description;
+    const imgfile = req.file.filename;
+    const newProduct = new Products({
+        title: title,
+        price: price,
+        description: description,
+        imgPath: imgfile
     })
-})
-newProduct.save(){
-    .then(result=>
-    res.status(200).json(result);
-    )}
 
-module.exports=router;
+    newProduct.save()
+    .then(result => {
+        res.status(200).json(result);
+    })
+    .catch(err => {
+        res.status(400).json(err);
+    })
+
+})
+
+module.exports = router;
