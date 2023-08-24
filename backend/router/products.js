c8fb66fc35a27f9b3aa24238e551c8e843daee27
guhan1203 <guhanramu1203@gmail.com>
2023-08-24 09:08:35 +0530

    back

---
+++ backend/router/products.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const router = express.Router();
+const multer = require("multer");
+const path = require("path");
+const Products = require("../model/product.model");
+
+const Storage = multer.diskStorage({
+    destination: function (req , file , cb){
+        cb (null,"public/images")
+    },
+    filename: function (req , file , cb){
+        cb (null,file,fieldname+"-",Date.now()+path.extname(file.originalname))
+    }
+})
+
+const upload =multer({storage:Storage})
+router.post("/add-product",upload,single("imageFile"),(req.res)=>){
+    const title=req.body.title;
+    const price=req.body.price;
+    const description=req.body.description;
+    const imageFile=req.file.filename;
+    const newProduct=new Products({
+        title:title,
+        price:price,
+        description:description,
+        imgPath:imageFile
+    })
+}
