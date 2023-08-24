55ec4304c3c13815525943594c0add53f3f5c303
guhan1203 <guhanramu1203@gmail.com>
2023-08-24 09:32:14 +0530

    js

--- backend/index.js
+++ backend/index.js
@@ -2,11 +2,16 @@ const express = require("express");
 
 const cors = require("cors");
 const mongoose = require("mongoose");
+const bodyParser = require("body-parser")
+const productRouter = require (" ./router/products")
 
 const app = express();
 app.use(cors());
+app.use(bodyParser.urlencoded({extended:false}))
+app.use(bodyParser.json())
 require("dotenv").config();
 const url = process.env.ATLAS_URL;
+app.use("/products",productRouter)
 
 app.use("/", (req, res)=>{
     res.send("Hello Node");
